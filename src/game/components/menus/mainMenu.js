f7486a36d5f53ba72cda00fd1e516dbf2aefc68b
Benjamin J Anderson <banderson@mashable.com>
2016-11-13 22:14:25 -0600

    Think of better subtitle

--- src/game/components/menus/mainMenu.js
+++ src/game/components/menus/mainMenu.js
@@ -8,7 +8,7 @@ export default (props) => {
   return (
     <div className='menu'>
       <h1>Hackable Mi!</h1>
-      <p>Teach Mi how to solve problems through code.</p>
+      <p>...</p>
       <div className='mainMenu'>
         <ul className='menuOptions'>
           <li>How to play</li>
